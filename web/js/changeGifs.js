1e6d67613ac1bec5a1e841b58dda22cc64e0b04a
形山竜一 <katatamaryuichi@Rim.local>
2020-10-11 17:06:00 +0900

    add: もう色々やりすぎて分からん

--- web/js/changeGifs.js
+++ web/js/changeGifs.js
@@ -3,8 +3,13 @@ var gifs_src = new Array(
   "./images/pacman.gif",
   "./images/bbb.gif"
 );
+var colors_bg = new Array("#c1c10f", "c42431", "1466c6");
+
 var num = -1;
 var screen = document.getElementById("gamemovie");
+var bg0 = document.getElementById("bg-0");
+var bg1 = document.getElementById("bg-1");
+var bg2 = document.getElementById("bg-0");
 
 slideshow_timer();
 
@@ -14,7 +19,10 @@ function slideshow_timer() {
   } else {
     num += 1;
   }
-  // screen.src = gifs_src[num];
-  // document.getElementById("gamemovie").src = gifs_src[num];
-  // setTimeout("slideshow_timer()", 4200);
+  screen.src = gifs_src[num];
+  bg0.style.backgroundColor = colors_bg[num];
+  bg1.style.backgroundColor = "black"; //colors_bg[num];
+  bg2.style.backgroundColor = colors_bg[num];
+
+  setTimeout("slideshow_timer()", 1000);
 }
