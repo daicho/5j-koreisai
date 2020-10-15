a0cb5511a4014ae4e63b232f84d767d360d40565
形山竜一 <katatamaryuichi@Rim.local>
2020-10-16 03:46:32 +0900

    fix: スクロールはば13インチのmacでだいたいいい感じになる様調節

--- web/js/jump.js
+++ web/js/jump.js
@@ -1,10 +1,10 @@
 const jump2ranking = () => {
-  scrollTo(0, 500);
+  scrollTo(0, 1050);
 };
 const jump2download = () => {
-  scrollTo(0, 1000);
+  scrollTo(0, 2000);
 };
 
 const jump2preview = () => {
-  scrollTo(0, 1500);
+  scrollTo(0, 15000);
 };
